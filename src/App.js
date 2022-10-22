0515e22859772ea9bc20e50ec33b8f19a8962270
Fahri Akbaba <fahriakbaba@gmail.com>
2022-10-22 06:29:22 +0300

    add to new note to store data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
- import Main from "./components/main/Main.jsx";
+ import React from "react";
+import Main from "./components/main/Main.jsx";
  import Sidebar from "./components/sidebar/Sidebar.jsx";
 
 
 function App() {
+  const [notes, setNotes] = React.useState([]);
+
+  const addNote = () => {
+    const newNote = {
+      id: new Date().getTime().toString(),
+      title: "Title",
+      body: "you can add"
+    }
+
+    setNotes(prevNotes => ([...prevNotes, newNote]));
+  }
+
+  console.log(notes);
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar addNote={addNote} />
       <Main /> 
     </div>
   );
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,35 @@
 import { BsPlusSquare } from 'react-icons/bs';
 import styles from "./Sidebar.module.css";
+import { RiDeleteBin3Line } from 'react-icons/ri';
 
-function Sidebar() {
+function Sidebar( {addNote} ) {
+    const deleteClick = () => {
+        console.log("delete click");
+    }
+
+
     return(
         <aside className={styles.container}>
             <header className={styles.header}>
                 <h1>notes</h1>
-                <BsPlusSquare className={styles.icon} />
+                <BsPlusSquare className={styles.icon} onClick={() => addNote()} />
             </header>
+            <ul className={styles.content}>
+            <li> 
+                <div className={styles.header}>
+                    <h5>title</h5>
+                    <button className={styles.btn} onClick={deleteClick} >
+                        <RiDeleteBin3Line className={styles.deleteIcon} />
+                    </button>
+                </div>
+                <p className={styles.context}>
+                    you can add
+                </p>
+                <p className={styles.footer}>
+                    history
+                </p>
+            </li>
+            </ul>
         </aside>
     )
 }
