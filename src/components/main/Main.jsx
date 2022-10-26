974674fa22e32ba15a6497e783a97e2a8c595874
Fahri Akbaba <fahriakbaba@gmail.com>
2022-10-26 06:47:40 +0300

    Main page layout is styled and rearranged

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,4 +1,6 @@
 import styles from "./Main.module.css";
+import ReactMarkdown from "react-markdown";
+
 
 function Main({ currentNote, updateNote }) {
   console.log(currentNote);
@@ -6,19 +8,19 @@ function Main({ currentNote, updateNote }) {
   const handleChange = (name, value) => {
     updateNote({
       ...currentNote,
-      [name]: value
-    })
+      [name]: value,
+    });
   };
 
   if (!currentNote) {
-    return <h3 className={styles.info}>No Active Note</h3>
+    return <h3 className={styles.info}>No Active Note</h3>;
   }
 
   return (
     <main className={styles.container}>
-      <section>
+      <section className={styles.form}>
         <input
-        className={styles.text}
+          className={styles.text}
           placeholder="Note Title"
           type="text"
           value={currentNote?.title}
@@ -27,11 +29,17 @@ function Main({ currentNote, updateNote }) {
         <textarea
           className={styles.textarea}
           cols="30"
-          rows="10"
+          rows="18"
           placeholder="Write your note here..."
           value={currentNote?.body}
           onChange={(e) => handleChange("body", e.target.value)}
         />
+      </section>
+      <section className={styles.markdownStyle}>
+        <h3>{currentNote.title}</h3>
+        <ReactMarkdown >
+          {currentNote?.body}
+        </ReactMarkdown>
       </section>
     </main>
   );
