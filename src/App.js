baa413749a75ac90bb8f23f5e63fd48effa5b133
Fahri Akbaba <fahriakbaba@gmail.com>
2022-10-26 21:10:09 +0300

    added localStorage to App component

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import Sidebar from "./components/sidebar/Sidebar.jsx";
 
 
 function App() {
-  const [notes, setNotes] = React.useState([]);
+  const [notes, setNotes] = React.useState(JSON.parse(localStorage.getItem("notes")) || []);
   const [activeNoteId, setActiveNoteId] = React.useState("");
+
+  React.useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes])
 
   const addNote = () => {
     const date = new Date();
@@ -17,10 +21,8 @@ function App() {
       body: "you can add",
       history: date.toLocaleString('en-US', options)
     }
-
     setNotes(prevNotes => ([...prevNotes, newNote]));
   }
-
   const deleteNote = (id) => {
     setNotes(prevNotes => {
       return prevNotes.filter(note => note.id !== id);
